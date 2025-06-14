ea2d7b78a5b8cb4f682b7f0c6a105d42e2ace3b6
Xero <juliuscaesar.caesar37@gmail.com>
2025-06-14 17:50:20 +0700

    add presence

--- apps/presence/src/main.ts
+++ apps/presence/src/main.ts
@@ -4,7 +4,8 @@ import { SharedService } from '@/shared';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(PresenceModule);
+  const app = await NestFactory.create(PresenceModule, { cors: true });
+  // app.enableCors();
 
   const configService = app.get(ConfigService);
   const sharedService = app.get(SharedService);
@@ -13,5 +14,7 @@ async function bootstrap() {
 
   app.connectMicroservice(sharedService.getRmqOptions(queue));
   app.startAllMicroservices();
+
+  await app.listen(6000);
 }
 bootstrap();
--- apps/presence/src/presence.module.ts
+++ apps/presence/src/presence.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
+
+import { SharedModule, RedisModule } from '@/shared';
+
 import { PresenceController } from './presence.controller';
 import { PresenceService } from './presence.service';
-import { RedisCacheService, RedisModule, SharedModule } from '@/shared';
-import { ConfigModule } from '@nestjs/config';
+import { PresenceGateway } from './presence.gateway';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: './.env',
-    }),
-
-    SharedModule,
     RedisModule,
+    SharedModule.registerRmq(
+      'AUTH_SERVICE',
+      process.env.RABBITMQ_AUTH_QUEUE || '',
+    ),
   ],
   controllers: [PresenceController],
-  providers: [PresenceService],
+  providers: [PresenceService, PresenceGateway],
 })
 export class PresenceModule {}
